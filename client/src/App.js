1b8328b32a6df97e9b4c5de221650f90cbfbea49
Hopp Willi <38337505+jhwillia7@users.noreply.github.com>
2018-11-18 17:07:35 -0800

    next version

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          
           <Switch>
             <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
