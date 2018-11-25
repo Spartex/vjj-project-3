c0564e6a9c97084bc09d5dcec3ebefa9802d8e40
Hopp Willi <38337505+jhwillia7@users.noreply.github.com>
2018-11-25 09:34:12 -0800

    latest 11-25-2018

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../../models')
+const db = require('../../models/user')
 const passport = require('../../passport')
 
 router.post('/', (req, res) => {
