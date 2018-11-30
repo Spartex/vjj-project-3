2527c79be66a0b9b7b5e3ffc173a6b4d0038cfa9
John Comodikes <johncomodikes@Johns-MacBook-Pro.local>
2018-11-29 20:07:10 -0800

    Adding css files

--- client/src/components/Group/IngredGroupCard.js
+++ client/src/components/Group/IngredGroupCard.js
@@ -4,7 +4,7 @@ import "./IngredGroupCard.css"
 export const IngredGroupCard = props => {
     return (
         <div key={props.type} className="card cardIngredGroup img-fluid" onClick = {props.modalOpen}>
-          <img className="card-img-top" src={props.typeImgURL} alt={props.type.toUpperCase()} />
+          <img className="card-img-top rounded-circle" src={props.typeImgURL} alt={props.type.toUpperCase()} />
           <div className="card-img-overlay">
             <h4 className="card-text">{props.type.toUpperCase()}</h4>
           </div>
--- client/src/components/Selection/getRecipeListBtn.js
+++ client/src/components/Selection/getRecipeListBtn.js
@@ -3,7 +3,7 @@ import "./getRecipeListBtn.css"
 
 export const GetRecipeListBtn = props => {
     return (
-        <button disabled={props.searching} onClick={props.action} className="btn btn-secondary getRecipeList btn-block">{props.searching ? "Searching..." : props.children}</button>
+        <button disabled={props.searching} onClick={props.action} className="btn btn-secondary getRecipeList btn-block searchbtn">{props.searching ? "Searching..." : props.children}</button>
       )
 }
 
